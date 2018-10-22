cd3250d3f77c24757d17e0e2326c80f677192b05
Lhure <maxjonilsson@gmail.com>
2018-10-22 13:20:40 +0200

    followed totorial with save and load

--- app/src/main/java/com/example/qbook/backyardbotany/SearchActivity.java
+++ app/src/main/java/com/example/qbook/backyardbotany/SearchActivity.java
@@ -2,7 +2,16 @@ package com.example.qbook.backyardbotany;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -11,10 +20,76 @@ public class SearchActivity extends AppCompatActivity {
 //        System.loadLibrary("native-lib");
 //    }
 
+    EditText mEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+    }
+    public void save(String str) {
+        FileOutputStream fos = null;
+
+
+
+        try {
+            fos = openFileOutput(str, MODE_PRIVATE);
+            fos.write(str.getBytes());
+
+            String absFilePath = getFilesDir() + "/" + str;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void load(String str) {
+        FileInputStream fis = null;
+
+        String absFilePath = getFilesDir() + "/" + str;
+
+
+        try {
+            fis = openFileInput(str);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String text;
+
+            while ((text = br.readLine()) != null)
+            {
+                sb.append(text).append("\n");
+            }
+
+            mEditText.setText(sb.toString());
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 
 //        // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
@@ -26,4 +101,4 @@ public class SearchActivity extends AppCompatActivity {
      * which is packaged with this application.
      */
     //public native String stringFromJNI();
-}
+
