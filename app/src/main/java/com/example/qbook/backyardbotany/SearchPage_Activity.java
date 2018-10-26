593844d0f736b56599605cb44948a38a90532a7e
JakubMakowiecki <209898@student.pwr.edu.pl>
2018-10-26 10:21:33 +0200

    Changed name of the app to BB Refactored the front page added the option to get writen text added a function to run search (empty) after enter and after clicking button.

--- app/src/main/java/com/example/qbook/backyardbotany/SearchPage_Activity.java
+++ app/src/main/java/com/example/qbook/backyardbotany/SearchPage_Activity.java
@@ -1,10 +1,16 @@
 package com.example.qbook.backyardbotany;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import java.util.Objects;
 
 
 public class SearchPage_Activity extends AppCompatActivity {
@@ -12,11 +18,41 @@ public class SearchPage_Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_page_);
+        setContentView(R.layout.activity_search_page);
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+        Button button = (Button) findViewById(R.id.button);
+        final EditText editText = (EditText) findViewById(R.id.editText);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String query = editText.getText().toString();
+                performSearch(query);
+                goToNextPage();
+                return false;
+            }
+        });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String query = editText.getText().toString();
+                performSearch(query);
+                goToNextPage();
+            }
+        });
+
+
     }
-        public void skip(View view){
-            Intent intent = new Intent();
-            intent.setClass(SearchPage_Activity.this,ResultPage_Activity.class);
-            startActivity(intent);
+
+    private void performSearch(String query) {
+        Log.d("searching for:", query);
     }
+
+    private void goToNextPage() {
+        Intent intent = new Intent();
+        intent.setClass(SearchPage_Activity.this, ResultPage_Activity.class);
+        startActivity(intent);
+    }
+
 }
