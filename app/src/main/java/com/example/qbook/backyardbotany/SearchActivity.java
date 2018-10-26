00972e6f9d0394466cb4d84385e9ad4f1f216d25
Lhure <maxjonilsson@gmail.com>
2018-10-26 10:45:05 +0200

    removed comment

--- app/src/main/java/com/example/qbook/backyardbotany/SearchActivity.java
+++ app/src/main/java/com/example/qbook/backyardbotany/SearchActivity.java
@@ -192,9 +192,5 @@ public class SearchActivity extends AppCompatActivity {
 //        tv.setText(stringFromJNI());
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    //public native String stringFromJNI();
+//public native String stringFromJNI();
 
