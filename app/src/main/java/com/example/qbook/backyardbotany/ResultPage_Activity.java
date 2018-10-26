5cd8b78c7249b603dd5a92c453de095242bf5560
JakubMakowiecki <209898@student.pwr.edu.pl>
2018-10-26 21:20:08 +0200

    Rewritten result page Added picture Added ScrollView Added Card layout Added sample text Changed Activity Title to sample name

--- app/src/main/java/com/example/qbook/backyardbotany/ResultPage_Activity.java
+++ app/src/main/java/com/example/qbook/backyardbotany/ResultPage_Activity.java
@@ -1,9 +1,9 @@
 package com.example.qbook.backyardbotany;
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
-
-import java.util.Objects;
+import android.support.v7.widget.Toolbar;
 
 public class ResultPage_Activity extends AppCompatActivity {
 
@@ -11,7 +11,10 @@ public class ResultPage_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result_page);
-        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
-
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        CollapsingToolbarLayout collapsingToolbarLayout = findViewById(R.id.toolbar_layout);
+        collapsingToolbarLayout.setTitleEnabled(true);
+        collapsingToolbarLayout.setTitle("Rose");
     }
 }
